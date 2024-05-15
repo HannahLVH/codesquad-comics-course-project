bc1387764524748889e518f654d8f842b3df43c9
Hannah Lopez <hannahlopez@Hannahs-Mac-mini.local>
2024-05-14 20:31:42 -0400

    fixing google and github strategy

--- config/authStrategy.js
+++ config/authStrategy.js
@@ -34,7 +34,7 @@ passport.use(
 passport.use(new GithubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/github",
+    callbackURL: "https://codesquad-comics-course-project.onrender.com/auth/github",
     },
     (accessToken, refreshToken, profile, done) => {
         console.log(profile);
